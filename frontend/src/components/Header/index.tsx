a083333e7077361e4240f21209480448b97d5658
oliveira-86 <egberto.duarte.14@gmail.com>
2021-04-12 21:05:52 -0300

    Add Header

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import './styles.css';
+import { ReactComponent as LogoImage } from '../../core/assets/logo.svg';
+
+const Header = () => (
+
+    <header className="main-header">
+        <LogoImage />
+        <div className="logo-text">
+            <span className="logo-text-1">Big Game</span>
+            <span className="logo-text-2"> Survey</span>
+        </div>
+    </header>    
+);
+
+export default Header;
