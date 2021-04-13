41ffad5d73d8424da333d8a5b280d41981b12eb6
oliveira-86 <egberto.duarte.14@gmail.com>
2021-04-12 21:58:28 -0300

    Add react-router-dom

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Header from './core/components/Header';
+import Home from './pages/Home';
+
+const Routes = () => (
+    <BrowserRouter>
+        <Header />
+        <Switch>
+            <Route path="/">
+                <Home />
+            </Route>
+        </Switch>
+    </BrowserRouter>
+);
+
+export default Routes;
